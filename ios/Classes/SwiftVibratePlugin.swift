854bad5c5334a118787ddcfc73ba2d5e8133b0e6
Đạt Hồ <dathdq@imt-soft.com>
2025-04-14 17:05:57 +0700

    Update SwiftVibratePlugin.swift
    Fix build ios xcode 16.3

--- ios/Classes/SwiftVibratePlugin.swift
+++ ios/Classes/SwiftVibratePlugin.swift
@@ -2,7 +2,7 @@ import Flutter
 import UIKit
 import AudioToolbox
 
-private let isDevice = TARGET_OS_SIMULATOR == 0
+private let isDevice = UIDevice.current.userInterfaceIdiom != .unspecified
     
 public class SwiftVibratePlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
